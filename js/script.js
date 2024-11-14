3a7ef2c3b33096b40873d6ca1261c898e1c45814
rodrigo7584 <waboo@waboo.com.br>
2024-11-14 11:44:42 -0300

    drag and drop

--- js/script.js
+++ js/script.js
@@ -2,29 +2,51 @@ document.addEventListener('DOMContentLoaded', function () {
   const colMenu = document.querySelector('.col-menu .menu')
   const colMap = document.querySelector('.col-map')
   const popUp = document.querySelector('.pop-up')
-  console.log(colMap)
-  console.log(colMenu)
+
   function adjustHeight() {
     if (colMenu && colMap) {
       const colMenuHeight = colMenu.offsetHeight
       colMap.style.maxHeight = colMenuHeight + 'px'
     }
   }
-  function initPopUp(){
-    if(colMap && popUp){
+
+  function initPopUp() {
+    if (colMap && popUp) {
       const closeButtons = popUp.querySelectorAll('.btn-close')
-      colMap.addEventListener
-      ('click', ()=>{
+
+      colMap.addEventListener('click', () => {
         popUp.classList.add('show')
       })
-      closeButtons.forEach(btn=>{
-          btn.addEventListener('click',()=>{
-            event.stopPropagation();
+      closeButtons.forEach(btn => {
+        btn.addEventListener('click', () => {
+          event.stopPropagation()
           popUp.classList.remove('show')
         })
       })
     }
   }
+
+  // Função de arraste
+  let offsetX,
+    offsetY,
+    isDragging = false
+
+  popUp.addEventListener('mousedown', e => {
+    isDragging = true
+    offsetX = e.clientX - popUp.offsetLeft
+    offsetY = e.clientY - popUp.offsetTop
+  })
+
+  document.addEventListener('mousemove', e => {
+    if (isDragging) {
+      popUp.style.left = `${e.clientX - offsetX}px`
+      popUp.style.top = `${e.clientY - offsetY}px`
+    }
+  })
+
+  document.addEventListener('mouseup', () => {
+    isDragging = false
+  })
 
   initPopUp()
   // Ajusta a altura quando o conteúdo é carregado
