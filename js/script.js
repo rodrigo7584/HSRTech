1ba4645df193198afb837dda4f6420a26157d189
rodrigo7584 <rodrigo.gandhi.oliveira@gmail.com>
2024-11-13 22:24:04 -0300

    popup

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
   const colMenu = document.querySelector('.col-menu .menu')
-  const colMapa = document.querySelector('.col-mapa')
-  console.log(colMapa)
+  const colMap = document.querySelector('.col-map')
+  const popUp = document.querySelector('.pop-up')
+  console.log(colMap)
   console.log(colMenu)
   function adjustHeight() {
-    if (colMenu && colMapa) {
+    if (colMenu && colMap) {
       const colMenuHeight = colMenu.offsetHeight
-      colMapa.style.maxHeight = colMenuHeight + 'px'
+      colMap.style.maxHeight = colMenuHeight + 'px'
+    }
+  }
+  function initPopUp(){
+    if(colMap && popUp){
+      const closeButtons = popUp.querySelectorAll('.btn-close')
+      colMap.addEventListener
+      ('click', ()=>{
+        popUp.classList.add('show')
+      })
+      closeButtons.forEach(btn=>{
+          btn.addEventListener('click',()=>{
+            event.stopPropagation();
+          popUp.classList.remove('show')
+        })
+      })
     }
   }
 
+  initPopUp()
   // Ajusta a altura quando o conteúdo é carregado
   adjustHeight()
 
